85e9f1158823c93e529bf04b817bac8861a31d01
Phong Nguyen <phonghoai743@gmail.com>
2026-03-12 17:18:23 +0700

    Initialize Express server with user authentication

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require("express")
+const session = require("express-session")
+const bodyParser = require("body-parser")
+const fs = require("fs")
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended:true }))
+
+app.use(session({
+secret:"portal",
+resave:false,
+saveUninitialized:true
+}))
+
+app.use(express.static("views"))
+
+function auth(req,res,next){
+if(req.session.user){
+next()
+}else{
+res.redirect("/login")
+}
+}
+
+app.get("/",(req,res)=>{
+res.sendFile(__dirname+"/views/index.html")
+})
+
+app.get("/register",(req,res)=>{
+res.sendFile(__dirname+"/views/register.html")
+})
+
+app.post("/register",(req,res)=>{
+
+let users = JSON.parse(fs.readFileSync("./data/users.json"))
+
+users.push({
+user:req.body.user,
+pass:req.body.pass
+})
+
+fs.writeFileSync("./data/users.json",JSON.stringify(users))
+
+res.redirect("/success")
+
+})
+
+app.get("/success",(req,res)=>{
+res.sendFile(__dirname+"/views/success.html")
+})
+
+app.get("/login",(req,res)=>{
+res.sendFile(__dirname+"/views/login.html")
+})
+
+app.post("/login",(req,res)=>{
+
+let users = JSON.parse(fs.readFileSync("./data/users.json"))
+
+let found = users.find(u => u.user === req.body.user && u.pass === req.body.pass)
+
+if(found){
+req.session.user = found.user
+res.redirect("/dashboard")
+}else{
+res.send("Wrong login")
+}
+
+})
+
+app.get("/dashboard",auth,(req,res)=>{
+res.sendFile(__dirname+"/views/dashboard.html")
+})
+
+app.listen(3000,()=>{
+console.log("Portal running on port 3000")
+})
